4aa9198d21906cc1924eb1ec4f0a1f057cb673c4
pktt26 <156344635+pktt26@users.noreply.github.com>
2026-05-24 23:19:22 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+import datetime
+import os 
+import uuid
+
+while True:
+    print("1- Abrir chamados")
+    print("2- Listar chamados") 
+    print("3- Fechar chamados")
+    print("4- Sair")
